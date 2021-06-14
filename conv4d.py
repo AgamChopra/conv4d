f2c33c656c7de24d20d5988abdd3df277e802666
Agam Chopra <work.agamchopra@gmail.com>
2021-06-14 18:30:47 -0400

    Update conv4d.py

--- conv4d.py
+++ conv4d.py
@@ -13,7 +13,7 @@ class BatchNorm4d(torch.nn.Module):
     
 def Conv4d(in_channels: int, out_channels: int, kernel_size:int=2, 
            stride:int=1, padding:int = 0, padding_mode: str ="zeros",  
-           bias: bool = True, groups: int = 1):
+           bias: bool = True, groups: int = 1, dilation: int = 1):
     w = torch.rand(1)[0]
     if bias:
         b = torch.zeros(1)[0]
@@ -21,13 +21,13 @@ def Conv4d(in_channels: int, out_channels: int, kernel_size:int=2,
                            num_dims=4,kernel_size=kernel_size, 
                            stride=(stride,stride,stride,stride), padding=padding, 
                            padding_mode=padding_mode, output_padding=0,
-                           is_transposed=False, use_bias=bias, groups=groups, 
+                           is_transposed=False, use_bias=bias, groups=groups, dilation = dilation, 
                            kernel_initializer=lambda x: torch.nn.init.constant_(x, w),  
                            bias_initializer=lambda x: torch.nn.init.constant_(x, b))
 
 def ConvTranspose4d(in_channels: int, out_channels: int, kernel_size:int=2,
                     stride:int=1, padding:int = 0, padding_mode: str ="zeros", 
-                    bias: bool = True, groups: int = 1):
+                    bias: bool = True, groups: int = 1, dilation: int = 1):
     w = torch.rand(1)[0]
     if bias:
         b = torch.zeros(1)[0]
@@ -35,6 +35,6 @@ def ConvTranspose4d(in_channels: int, out_channels: int, kernel_size:int=2,
                            num_dims=4,kernel_size=kernel_size, 
                            stride=(stride,stride,stride,stride), padding=padding, 
                            padding_mode=padding_mode, output_padding=0,
-                           is_transposed=True, use_bias=bias, groups=groups, 
+                           is_transposed=True, use_bias=bias, groups=groups,  dilation = dilation, 
                            kernel_initializer=lambda x: torch.nn.init.constant_(x, w),  
                            bias_initializer=lambda x: torch.nn.init.constant_(x, b))
